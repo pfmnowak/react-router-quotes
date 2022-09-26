afcbef79787cf3e8f88eee8b302ca623464d949b
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-09-26 23:14:12 +0200

    Add LAyout wrapper and navigation

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Comments from "./components/comments/Comments";
+import Layout from "./components/layout/Layout";
 import AllQuotes from "./components/pages/AllQuotes";
 import NewQuote from "./components/pages/NewQuote";
 import QuoteDetail from "./components/pages/QuoteDetail";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Navigate to="/quotes" replace />} />
-      <Route path="/quotes" element={<AllQuotes />} />
-      <Route path="/quotes/:quoteId" element={<QuoteDetail />}>
-        <Route path="/quotes/:quoteId/comments" element={<Comments />} />
-      </Route>
-      <Route path="/new-quote" element={<NewQuote />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Navigate to="/quotes" replace />} />
+        <Route path="/quotes" element={<AllQuotes />} />
+        <Route path="/quotes/:quoteId" element={<QuoteDetail />}>
+          <Route path="/quotes/:quoteId/comments" element={<Comments />} />
+        </Route>
+        <Route path="/new-quote" element={<NewQuote />} />
+      </Routes>
+    </Layout>
   );
 }
 
