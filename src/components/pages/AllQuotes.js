92df04b22ea5fcc4a42b76280c8bc2ffab4a9090
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-09-26 23:26:32 +0200

    Add dummy data and quote form

--- src/components/pages/AllQuotes.js
+++ src/components/pages/AllQuotes.js
@@ -1,7 +1,21 @@
 import React from "react";
+import QuoteList from "../quotes/QuoteList";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Mik",
+    text: "Learning React is great!",
+  },
+];
 
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
