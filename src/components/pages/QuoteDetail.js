2d70c4e7857e0d8994dcad00076d9d06c5af2dfb
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-09-27 08:26:36 +0200

    Add link to quote details, display quote detilas

--- src/components/pages/QuoteDetail.js
+++ src/components/pages/QuoteDetail.js
@@ -1,12 +1,32 @@
 import React from "react";
 import { Outlet, useParams } from "react-router-dom";
+import HighlightedQuote from "../quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  {
+    id: "q1",
+    author: "Max",
+    text: "Learning React is fun!",
+  },
+  {
+    id: "q2",
+    author: "Mik",
+    text: "Learning React is great!",
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
+
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote {...quote} />
 
       <Outlet />
     </>
