c5ef5dcf2a556893b4c4ffd19b17807e76f72151
pfmnowak <58272232+pfmnowak@users.noreply.github.com>
2022-09-27 09:08:09 +0200

    Implement programmatic navigation

--- src/components/pages/NewQuote.js
+++ src/components/pages/NewQuote.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import QuoteForm from "../quotes/QuoteForm";
 
 const NewQuote = () => {
+  const navigate = useNavigate();
+
   const addQuoteHandler = (quoteData) => {
     console.log(quoteData);
+
+    navigate("/quotes");
   };
 
   return <QuoteForm onAddQuote={addQuoteHandler} />;
